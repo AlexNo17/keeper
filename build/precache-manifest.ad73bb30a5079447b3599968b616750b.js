d48b0cf2855e6d6ac56316478adb39c48ca4c55a
Alex <alex@surfin.sg>
2024-02-20 14:51:22 +0800

    hello

---
+++ build/precache-manifest.ad73bb30a5079447b3599968b616750b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "664fddf4aa24985f6fbab6d5d1e982bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ce86e59b001bd93c728",
+    "url": "/static/js/2.cd46f1df.chunk.js"
+  },
+  {
+    "revision": "b60768c5012492e661c5",
+    "url": "/static/js/main.8d35c4f1.chunk.js"
+  },
+  {
+    "revision": "2535e277f6499596031f",
+    "url": "/static/js/runtime-main.9c1af6e4.js"
+  }
+]);
